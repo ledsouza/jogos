b23e73aed5f249d30e82bb7e383c38affd9c4c12
Leandro Souza <leandro.souza.159@gmail.com>
2023-06-28 12:50:12 -0300

    Manipulando strings

--- forca.py
+++ forca.py
@@ -10,7 +10,14 @@ def jogar():
     acertou = False
 
     while(not enforcou and not acertou):
-        # jogando...
+        chute  = input('Qual letra? ')
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index += 1
 
 if(__name__ == "__main__"):
     jogar()
