5264bd68bba131d828c29df45ff077c8ca6bc19a
Leandro Souza <leandro.souza.159@gmail.com>
2023-06-28 10:59:38 -0300

    Preparando o jogo da forca

--- forca.py
+++ forca.py
@@ -5,5 +5,12 @@ def jogar():
 
     print("Fim do jogo")
 
+    palavra_secreta = 'banana'
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        # jogando...
+
 if(__name__ == "__main__"):
     jogar()
